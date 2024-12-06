437ef7998cb91e0818567a7585255c990556f152
Garvit Khanna <garvitkhanna1.41@gmail.com>
2024-12-07 04:35:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def submit_file():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000)) # Define port so we can map container port to localhost.
+    app.run(host='0.0.0.0', port=port)  # Define 0.0.0.0 for Docker.
